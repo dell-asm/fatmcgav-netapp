ab9a0bcceecbc8bca959fab20e5b507a419a8d4d
saurabh misra <saurabh_misra@dell.com>
2013-12-30 04:39:18 +0000

    Updated type for removing unwanted parameters

--- lib/puppet/type/netapp_lun_clone_unclone.rb
+++ lib/puppet/type/netapp_lun_clone_unclone.rb
@@ -15,20 +15,9 @@ Puppet::Type.newtype(:netapp_lun_clone_unclone) do
   newparam(:parentsnap) do  
   end
   
-  newparam(:snapshot) do  
-  end
-  
-  newparam(:volume) do
-  end
-
   newparam(:spacereservationenabled, :boolean => false) do
     newvalues(:true, :false)
     defaultto :false
   end
 
-  newparam(:force, :boolean => false) do
-    newvalues(:true, :false)
-    defaultto :false
-  end
-  
 end
